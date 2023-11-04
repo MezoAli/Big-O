6648f60a813b4feac6ff519454689c342f385b83
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-04 14:48:54 +0200

    add performance now to measure scalbility

--- big-o-notation/main.js
+++ big-o-notation/main.js
@@ -2,6 +2,7 @@ const nemo = ["nemo"];
 const mezo = ["mezo"];
 
 const findNemo = (array) => {
+  const t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       console.log("we found nemo");
@@ -9,7 +10,8 @@ const findNemo = (array) => {
       console.log("we can't find nemo");
     }
   }
+  const t1 = performance.now();
+  console.log(`the function took ${t1 - t0} ms to find nemo`);
 };
 
 findNemo(nemo);
-findNemo(mezo);
