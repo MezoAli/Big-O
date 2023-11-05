cdb021fabe3faa5c4add8b5001f7f75ed1e80adf
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-05 10:03:09 +0200

    create func to log all pairs together

--- big-o-notation/main.js
+++ big-o-notation/main.js
@@ -27,20 +27,37 @@
 // printFirst2Boxes(boxes); // o(1) => const time
 
 // FIRST CHALLANGE
-const anotherFunction = () => {};
+// const anotherFunction = () => {};
 
-const funChallenge = (inputs) => {
-  let a = 10; // o(1)
-  a = 50 + 3; // o(1)
+// const funChallenge = (inputs) => {
+//   let a = 10; // o(1)
+//   a = 50 + 3; // o(1)
 
-  for (let i = 0; i < inputs.length; i++) {
-    //o(n)
-    anotherFunction(); //o(n)
-    let stranger = true; //o(n)
-    a++; //o(n)
+//   for (let i = 0; i < inputs.length; i++) {
+//     //o(n)
+//     anotherFunction(); //o(n)
+//     let stranger = true; //o(n)
+//     a++; //o(n)
+//   }
+//   return a; //o(1)
+// };
+
+// console.log(funChallenge([1, 2, 3, 4, 5, 6])); // o(3 + 4n) => o(n)
+// console.log(funChallenge([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // o(3 + 4n) => o(n)
+
+// log all pairs together
+
+const array = [1, 2, 3, 4, 5, 6];
+let total = 0;
+
+const logPairs = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+      total++;
+    }
   }
-  return a; //o(1)
 };
 
-console.log(funChallenge([1, 2, 3, 4, 5, 6])); // o(3 + 4n) => o(n)
-console.log(funChallenge([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // o(3 + 4n) => o(n)
+logPairs(array);
+console.log(`the function run ${total} times`);
