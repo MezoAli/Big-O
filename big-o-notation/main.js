dfcbd56a6cc9b9bb51e78fa8ca727e3c130f40f5
Moutaz Ali <moutaz.ali.dev@gmail.com>
2023-11-07 11:24:36 +0200

    add 2 same purpose functions with 2 different Big O

--- big-o-notation/main.js
+++ big-o-notation/main.js
@@ -47,17 +47,77 @@
 
 // log all pairs together
 
-const array = [1, 2, 3, 4, 5, 6];
-let total = 0;
+// const array = [1, 2, 3, 4, 5, 6];
+// let total = 0;
 
-const logPairs = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      console.log(array[i], array[j]);
-      total++;
+// const logPairs = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//       console.log(array[i], array[j]);
+//       total++;
+//     }
+//   }
+// };
+
+// logPairs(array);
+// console.log(`the function run ${total} times`);
+
+// compare two functions with different big O
+
+const array1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+const array2 = [
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "a",
+];
+
+const compareCommonValues = (arr1, arr2) => {
+  let t1 = performance.now();
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        let t2 = performance.now();
+        console.log(`function took ${t2 - t1} ms to perform`);
+        return true;
+      }
     }
   }
+  t2 = performance.now();
+  console.log(`function took ${t2 - t1} ms to perform`);
+  return false;
 };
 
-logPairs(array);
-console.log(`the function run ${total} times`);
+console.log(compareCommonValues(array1, array2)); // Big O (a*b)
+
+const compareCommonValues2 = (arr1, arr2) => {
+  let t1 = performance.now();
+  let obj = {};
+  for (let i = 0; i < arr1.length; i++) {
+    obj[arr1[i]] = true;
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (obj[arr2[j]]) {
+      let t2 = performance.now();
+      console.log(`function took ${t2 - t1} ms to perform`);
+      return true;
+    }
+  }
+  t2 = performance.now();
+  console.log(`function took ${t2 - t1} ms to perform`);
+  return false;
+};
+
+console.log(compareCommonValues2(array1, array2)); // Big O (a+b)
